ee6bd36cbe86684ad4043d658be8aafb9b153b72
Divyank Jain Singhvi <119522879+divyank-jain-singhvi@users.noreply.github.com>
2023-12-25 14:30:41 +0530

    update-1

---
+++ extracting_human_voice1.py
@@ -0,0 +1,161 @@
+from pydub import AudioSegment
+import os
+import wave
+import numpy as np
+import librosa
+import soundfile as sf
+from firebase_admin import storage,credentials
+import firebase_admin
+from pydub.playback import play
+import subprocess 
+
+
+
+count=0
+
+def mp3_to_wav(filename):
+    replaced_filename=filename.replace('mp3','wav')
+    subprocess.call(['ffmpeg', '-i','./user audio data/'+filename, 
+    				'./user audio data/'+replaced_filename])
+
+
+def play_audio(play_file):
+    print("playing")
+    audio = AudioSegment.from_file(play_file)
+    play(audio)
+
+def firebase_upload(file_path):
+    bucket = storage.bucket()
+    try:
+        blob = storage.bucket().blob(file_path.filename)
+        blob.upload_from_string(file_path.read())
+    except (AttributeError):
+        blob = storage.bucket().blob('extracted.wav')
+        blob.upload_from_filename('./user audio data/'+file_path)
+        
+
+
+def firebase_extraction(file_path):
+    dir_path = './user audio data'
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+    bucket = storage.bucket()
+    blob = bucket.blob(file_path)
+    local_file_path = os.path.join(dir_path, os.path.basename(file_path))
+    blob.download_to_filename(local_file_path)
+    
+    # a = plot_audio_waveform(local_file_path)
+
+
+def split_audio(input_file, output_prefix, segment_length_ms):
+    global count
+    count=0
+    audio = AudioSegment.from_file(input_file)
+
+
+    num_segments = len(audio) // segment_length_ms
+    # print(num_segments)
+
+    for i in range(num_segments):
+        
+        start_time = i * segment_length_ms
+        end_time = (i + 1) * segment_length_ms
+        segment = audio[start_time:end_time]
+
+        output_file = f"{output_prefix}_{i + 1}.wav"
+        segment.export("./audio_segment/"+output_file, format="wav")
+        count+=1
+    return count
+
+
+
+def find_time_of_max_amplitude(audio_path):
+    # Step 2: Load the Audio File
+    audio = AudioSegment.from_wav(audio_path)
+
+    # Step 3: Convert to NumPy Array
+    audio_data = np.array(audio.get_array_of_samples())
+    
+
+    # Step 4: Find the Time of Maximum Amplitude
+    max_amplitude_index = np.argmax(np.abs(audio_data))
+
+    time_of_max_amplitude = max_amplitude_index / audio.frame_rate
+    return time_of_max_amplitude
+
+def count_variable_x(main_max_amplitude_list):
+    global count
+    main_max_amplitude_list.sort()
+    midpoint=len(main_max_amplitude_list)//2
+
+    first_part = main_max_amplitude_list[:midpoint]
+    second_part = main_max_amplitude_list[midpoint:]
+    print(first_part,second_part)
+        
+    
+    mid = len(first_part) // 2
+    if (mid*2)%2==0:
+        Q1 = (first_part[mid-1] + first_part[mid]) / 2
+    else:
+        Q1 = (first_part[mid+1]) / 2
+    mid1 = len(second_part) // 2
+    if (mid1*2)%2==0:
+        Q3 = (second_part[mid1-1] + second_part[mid1]) / 2
+    else:
+        Q3 = (second_part[mid1]) / 2
+    print(Q3,second_part[mid1],second_part[mid1])
+    
+    IQR=Q3-Q1
+    print(IQR)
+    IQR=IQR*1.5  #99.7 accuracy
+    print(IQR)
+    Q1=Q1-IQR
+    Q3=Q3+IQR
+    print(Q1,Q3)
+    if  (Q1<0 and Q3>20000):
+        return Q3+Q1,second_part
+    else:
+        return IQR,second_part
+
+def plot_audio_waveform(audio_file_path):
+
+    with wave.open(audio_file_path, 'rb') as wf:
+
+        framerate = wf.getframerate()
+        nframes = wf.getnframes()
+
+
+        audio_data = wf.readframes(nframes)
+
+
+    audio_array = np.frombuffer(audio_data, dtype=np.int16)
+    # for i in audio_array:
+    #     print(i)
+
+   # time_values = np.arange(0, nframes) / framerate
+
+    # plt.figure(figsize=(10, 4))
+    # plt.plot(time_values, audio_array, color='b')
+    # plt.title('Audio Waveform')
+    # plt.xlabel('Time (s)')
+    # plt.ylabel('Amplitude')
+    # plt.grid(True)
+    # plt.show()
+    print(audio_array)
+    return max(audio_array)
+
+
+
+def change_amplitude(audio_file_path, start_time, end_time, scaling_factor, output_file_path):
+    # Step 1: Load the Audio File
+    signal, sample_rate = librosa.load(audio_file_path, sr=None)
+
+    # Step 2: Select the Time Frame
+    start_sample = int(start_time * sample_rate)
+    end_sample = int(end_time * sample_rate)
+
+    # Step 3: Modify Amplitude
+    signal[start_sample:end_sample] *= scaling_factor
+
+    # Step 4: Save Modified Audio using soundfile
+    sf.write(output_file_path, signal, sample_rate)
---
+++ main1.py
@@ -0,0 +1,177 @@
+from flask import Flask, render_template,request, redirect, url_for,send_file
+import wave
+import struct
+import os
+from extracting_human_voice1 import *
+import shutil
+
+
+
+
+app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+duration = 15
+answer=''
+sample_rate = 44100
+time_frame_counter=0
+file_path=''
+filename=''
+convert_flag=0
+
+cred = credentials.Certificate("voice-extraction-firebase-adminsdk-mxc5w-ec1c227e24.json")
+firebase_admin.initialize_app(cred, {"storageBucket": "voice-extraction.appspot.com"})
+
+
+def record(filename,filename_extracted):
+    global time_frame_counter
+    if not os.path.exists('./audio extracted data'):
+        print('dir 1')
+        os.mkdir('./audio extracted data')
+    if not os.path.exists('./audio_segment'):
+        print('dir 1')
+        os.mkdir('./audio_segment')
+# Open the file
+    WAVE_OUTPUT_FILENAME = filename
+    waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'rb')
+    
+    # Get the properties of the audio file
+    CHANNELS = waveFile.getnchannels()
+    RATE = waveFile.getframerate()
+    SAMPLE_WIDTH = waveFile.getsampwidth()
+    
+    # Read the frames
+    frames = waveFile.readframes(-1)
+    
+    # Close the file
+    waveFile.close()
+    
+    
+    count=0
+    main_max_amplitude_list=[]
+    max_amplitude_time_list=[]
+    main_list=[]
+    input_file = WAVE_OUTPUT_FILENAME
+    output_prefix = 'output_segment'
+    segment_length_ms = 1000
+    main_amplitude_split=[]
+    count=split_audio(input_file, output_prefix, segment_length_ms)
+    # a=plot_audio_waveform(input_file)
+    for i in range(0,count):
+        audio_file_segment_path = f'./audio_segment/output_segment_{i+1}.wav'
+        max_audio_amplitude=plot_audio_waveform(audio_file_segment_path)
+        main_max_amplitude_list.append(max_audio_amplitude)
+        main_amplitude_split.append(max_audio_amplitude)
+        print(max_audio_amplitude)
+        max_amplitude_time = find_time_of_max_amplitude(audio_file_segment_path)
+        print(f"Time of maximum amplitude: {max_amplitude_time} seconds")
+        max_amplitude_time_list.append(max_amplitude_time)
+    print(main_max_amplitude_list)
+    variable_x,second_list=count_variable_x(main_max_amplitude_list)
+    print(variable_x)
+    duration=int(request.values.get('filesize'))
+    while True:    
+        for i in range(0,count):
+            if main_amplitude_split[i]<=variable_x:
+                time_frame_counter=(time_frame_counter+1)%duration
+            else:
+                main_list.append(time_frame_counter)
+                time_frame_counter=(time_frame_counter+1)%duration
+        if main_amplitude_split[i] >variable_x:
+            main_list.append(time_frame_counter)
+            
+        if main_list!=[]:
+            break
+        else:
+            variable_x=second_list[0]
+    print(main_max_amplitude_list)
+    print(max_amplitude_time_list)
+    print(main_list)
+    
+        
+    try:
+        start_time = 0.0  # start time in seconds
+        end_time = main_list[0]  # end time in seconds
+        scaling_factor = 0.001   # adjust this value to change amplitude
+        output_file_path = WAVE_OUTPUT_FILENAME
+        
+        change_amplitude(input_file, start_time, end_time, scaling_factor, output_file_path)
+        
+        for i in range(0,len(main_list)):
+    
+            start_time = main_list[i]+1  # start time in seconds
+            if i==len(main_list)-1:
+                end_time=duration
+            else:
+                end_time = main_list[i+1]
+            print(start_time,end_time)
+            change_amplitude(output_file_path, start_time, end_time, scaling_factor, output_file_path)
+        print(start_time,end_time)
+        change_amplitude(output_file_path, start_time, end_time, scaling_factor, output_file_path)
+    except IndexError:
+        pass
+    firebase_upload(filename_extracted)
+    print('upload',filename_extracted)
+    
+    if os.path.exists('./audio extracted data/'):
+        shutil.rmtree('./audio extracted data/')
+    if os.path.exists('./audio_segment/'):
+        shutil.rmtree('./audio_segment/')
+    firebase_extraction('extracted.wav')
+    
+    
+    # play_audio(WAVE_OUTPUT_FILENAME)
+    # a=plot_audio_waveform(input_file)
+    
+    
+    
+    # a=plot_audio_waveform(WAVE_OUTPUT_FILENAME)
+    
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def index1():
+    global file_path,filename
+    file_path = request.files['file']
+    filename=file_path.filename
+    firebase_upload(file_path)
+    print('upload',file_path)
+    firebase_extraction(filename)
+    print('extract',filename)
+    
+    if '.mp3' in filename:
+        mp3_to_wav(filename)
+        filename=filename.replace('.mp3','.wav')
+    print('mp3-->wav')
+    
+    
+    user_audio_file ='./user audio data/'+filename
+    record(user_audio_file,filename)
+    
+    result='./user audio data/extracted.wav'
+    return render_template("index1.html",result=result)
+
+# @app.route('/downloard', methods=['GET', 'POST'])
+# def index2():
+#     global filename
+#     firebase_extraction('extracted.wav')
+#     return render_template("index.html")
+#     # return render_template("index.html")
+
+@app.route('/download')
+def download_file():
+    # Specify the path to the file you want to serve for download
+    path = "./user audio data/extracted.wav"
+    return send_file(path, as_attachment=True)
+
+
+@app.route('/home', methods=['GET', 'POST'])
+def index3():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
